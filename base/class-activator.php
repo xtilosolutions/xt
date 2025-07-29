23debda36cab591145cb1730ca4fcea647cdd359
xtilosolutions <xtilosolutions@gmail.com>
2025-07-29 02:20:43 -0400

    Add files via upload

---
+++ base/class-activator.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://xplodedthemes.com
+ * @since      1.0.0
+ *
+ * @package    XT_Woo_Quick_View
+ * @subpackage XT_Woo_Quick_View/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    XT_Woo_Quick_View
+ * @subpackage XT_Woo_Quick_View/includes
+ * @author     XplodedThemes 
+ */
+class XT_Woo_Quick_View_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+		
+	}
+
+}
+
+XT_Woo_Quick_View_Activator::activate();
---
+++ base/class-uninstaller.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Fired during plugin uninstall
+ *
+ * @link       http://xplodedthemes.com
+ * @since      1.0.0
+ *
+ * @package    XT_Woo_Quick_View
+ * @subpackage XT_Woo_Quick_View/includes
+ */
+
+/**
+ * Fired during plugin uninstall.
+ *
+ * This class defines all code necessary to run during the plugin's uninstall.
+ *
+ * @since      1.0.0
+ * @package    XT_Woo_Quick_View
+ * @subpackage XT_Woo_Quick_View/includes
+ * @author     XplodedThemes 
+ */
+class XT_Woo_Quick_View_Uninstaller {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function uninstall() {
+		
+	}
+
+}
+
+XT_Woo_Quick_View_Uninstaller::uninstall();
